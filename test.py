7f63239a617aec9a336c7667d315c69125452868
wg4568 <wg4568@gmail.com>
2017-02-22 19:57:44 -0800

    changed test.py again

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import connections, random
 
+def random_num(min, max):
+	return random.randint(min, max)
+
 server = connections.Server(host="0.0.0.0", port=5000, debug=True)
 
-server.add_link("random", random.randint)
+server.add_link("random", random_num)
 
 server.start()
